2fd1a12529e24c13da3f733706a11ed463f159ae
Ajit <ageorge@drw.com>
2011-12-07 15:03:54 -0600

    reformat

--- ageorge.el
+++ ageorge.el
@@ -48,22 +48,21 @@
 
 ;; Flymake/pyflakes
 (when (load "flymake" t)
- (defun flymake-pyflakes-init ()
- (let* ((temp-file (flymake-init-create-temp-buffer-copy
- 'flymake-create-temp-inplace))
- (local-file (file-relative-name
- temp-file
- (file-name-directory buffer-file-name))))
- (list "pyflakes" (list local-file))))
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
 
- (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pyflakes-init)))
+  (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pyflakes-init)))
 
 (add-hook 'python-mode-hook 
-      (lambda () 
-        (unless (eq buffer-file-name nil) (flymake-mode 1)) ;dont invoke flymake on temporary buffers for the interpreter
-        (local-set-key [f2] 'flymake-goto-prev-error)
-        (local-set-key [f3] 'flymake-goto-next-error)
-        ))
+          (lambda () 
+            (unless (eq buffer-file-name nil) (flymake-mode 1)) ;dont invoke flymake on temporary buffers for the interpreter
+            (local-set-key [f2] 'flymake-goto-prev-error)
+            (local-set-key [f3] 'flymake-goto-next-error)))
 
 ;; Uniquify
 (setq uniquify-buffer-name-style 'reverse)
