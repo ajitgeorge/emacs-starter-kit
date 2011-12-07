7ffbafa3d94528592662c1f166c05993ddfb60b0
Ajit <ageorge@drw.com>
2011-12-07 09:20:37 -0600

    Set color to white on black.  Ropemacs works

--- ageorge.el
+++ ageorge.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 93 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 93 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 (global-set-key [C-f9] 'compile)
 ;; (global-set-key [C-c C-/] 'comment-or-uncomment-region)
@@ -32,4 +32,10 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+;; python
+; sudo apt-get install python-ropemacs
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")   
+(setq ropemacs-enable-autoimport t)
 
